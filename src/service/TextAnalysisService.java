b6ee575705d085f9fe3b790b3f83ec72d8e3bda2
mustafak01 <mestefe119@gmail.com>
2025-12-10 17:00:55 +0100

    edit TextAnalysisService wordFrequency method

--- src/service/TextAnalysisService.java
+++ src/service/TextAnalysisService.java
@@ -4,8 +4,23 @@ import core.map.HMap;
 
 public class TextAnalysisService {
 
-    public HMap<String,Integer> wordFrequency(String word){
-        return new HMap<>();
+    public Integer wordFrequency(String mainText, String searchedWord){
+        if (searchedWord == null || searchedWord.isEmpty()) return 0;
+        searchedWord = searchedWord.trim().toLowerCase();
+        String cleanText = mainText.replaceAll("[^a-zA-Z0-9\\s]", "").toLowerCase();
+        String[] words = cleanText.split("\\s+");
+        HMap<String,Integer> map = new HMap<>();
+        for (String w : words) {
+            if(!w.isEmpty()){
+                if(map.getVal(w)!=null){
+                    map.put(w,map.getVal(w)+1);
+                }else{
+                    map.put(w,1);
+                }
+            }
+        }
+        System.out.println(map.getVal(searchedWord));
+        return map.getVal(searchedWord);
     }
 
 
