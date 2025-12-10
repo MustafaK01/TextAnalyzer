d0474e03ce2f21bc3e9933b247e8d5dfad5d3855
mustafak01 <mestefe119@gmail.com>
2025-12-10 01:58:48 +0100

    add TextAnalysisService

---
+++ src/service/TextAnalysisService.java
@@ -0,0 +1,12 @@
+package service;
+
+import core.map.HMap;
+
+public class TextAnalysisService {
+
+    public HMap<String,Integer> wordFrequency(String word){
+        return new HMap<>();
+    }
+
+
+}
